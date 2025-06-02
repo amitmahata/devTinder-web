ae9a75b411a02ce217a88be163e171748203a4d7
amitmahata <00.amitmahata37@gmail.com>
2025-06-02 19:46:24 +0530

    routing

---
+++ src/Body.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar"
+import Footer from './Footer'
+import { Outlet } from 'react-router-dom'
+
+export const Body = () => {
+    return (
+        <div>
+            <NavBar />
+            <Outlet />
+            <Footer />
+        </div>
+    )
+}
