b29fbe7e7cd80000bd50ea1bdcfa726c7e402d38
amitmahata <00.amitmahata37@gmail.com>
2025-06-04 00:13:52 +0530

    initial feed and user-card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useEffect } from 'react'
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useSelector } from "react-redux";
+import UserCard from './UserCard';
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+  const getFeed = async () => {
+    if (feed) return;
+
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
   return (
-    <div>Feed</div>
-  )
+    feed && (
+      <div className='flex flex-wrap gap-5 justify-center my-5'>
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
 }
 
 export default Feed
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const UserCard = ({ user }) => {
+    const { photoUrl, firstName, lastName, about, age, gender, skills } = user;
+    console.log(user);
+    return (
+        <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+                <img src={photoUrl} alt="Photo" className=''/>
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{firstName} {lastName}</h2>
+                <p>{age} {gender}</p>
+                <p>{about}</p>
+                <div className="card-actions justify-center">
+                    <button className="btn btn-primary">Ignore</button>
+                    <button className="btn btn-secondary">Interested</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard
